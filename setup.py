19fc63b5c7f5dbcf973e1a361ff4678bbf44febd
Alix Chagué <alix.chague@inria.fr>
2023-09-04 20:58:51 +0200

    fix dependencies parsing

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ HERE = pathlib.Path(__file__).parent
 # The text of the README file
 README = (HERE/"README.md").read_text()
 
-with open("requirements.txt", encoding="utf-8") as f:
-    install_required = f.read().splitlines()
+#with open("requirements.txt", encoding="utf-8") as f:
+#    install_required = f.read().splitlines()
 
 setup(
     name='boombox',
@@ -22,7 +22,14 @@ setup(
     author = "Alix Chagué",
     license='MIT',
     python_requires='>=3.8',
-    install_requires=install_required,
+    install_requires=[
+        "lxml==4.9.2",
+        "textdistance==4.5.0",
+        "typo @ git+https://github.com/alix-tz/typo@b2f65a418c0671ce749ab4cb060acfdb2e6062fa",
+        "tabulate==0.9.0",
+        "art==5.8",
+        "pandas==1.3.5",
+    ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
